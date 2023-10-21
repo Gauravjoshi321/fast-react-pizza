f7ad9f73fb8223767f753025ef3e58b3d4d9aa74
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-10-21 19:37:35 +0530

    error handling with react-router

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  console.log(navigation)
+  const loading = navigation.state === "loading";
+
+
   return (
     <div className="layout">
+      {loading && <Loader />}
       <Header />
 
       <Outlet />
