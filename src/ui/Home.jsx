c9c314908793b6350907740e66405a4d135dd14f
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-10-26 19:47:28 +0530

    stylee

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,15 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-xl text-center text-yellow-500 font-semibold">
+    <div className="text-center my-10">
+      <h1 className="text-xl text-yellow-500 font-semibold mb-6">
         <span className="text-stone-700">The best pizza.</span>
         <br />
         Straight out of the oven, straight to you.
       </h1>
+
+      <CreateUser />
     </div>
   );
 }
