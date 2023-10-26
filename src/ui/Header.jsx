3b233250c76c47a24ffb5844445355702edbdce8
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-10-26 20:03:08 +0530

    responsive

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase px-4 py-3 border-stone-200 border-b-2">
+    <header className="bg-yellow-500 uppercase px-4 py-3 sm:px-6 border-stone-200 border-b-2">
       <Link to="/" className="tracking-widest"> Fast React Pizza co.</Link>
       <SearchOrder />
       <Username />
