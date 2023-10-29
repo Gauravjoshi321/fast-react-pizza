2d573f8ddda08ae293104535e54fbcb60b3d52f2
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-10-29 15:24:57 +0530

    search order style

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -16,9 +16,19 @@ function SearchOrder() {
     <form onSubmit={(e) => handleSubmit(e)}>
       <input
         type="text"
-        placeholder="Serach Order"
+        placeholder="Search Order"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="
+          w-28
+          rounded-full 
+          px-3 py-2 
+          text-sm 
+          placeholder:text-stone-500 
+          bg-yellow-100 
+          focus:outline-none focus:ring focus:ring-yellow-600 focus:ring-opacity-20 sm:focus:w-72 transition-all duration-300
+          sm:w-56
+          "
       />
     </form>
   )
