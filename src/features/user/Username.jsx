1022a4f38e684d9f555a77d14db41184d35736ba
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-02 19:18:08 +0530

    userName

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux"
+
 export default function Username() {
+
+  const userName = useSelector(state => state.user.userName);
+  if (!userName) return null;
+
   return (
-    <div className="text-sm font-semibold hidden sm:block">Jonas</div>
+    <div className="text-sm font-semibold hidden sm:block">{userName}</div>
   )
 }
