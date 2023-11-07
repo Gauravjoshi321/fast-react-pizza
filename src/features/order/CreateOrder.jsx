5914b23ecb6b4dc914031b7606f3f9255919e780
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-07 10:59:19 +0530

    hidden position

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -17,22 +17,20 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-
-  const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
-
   const formErrors = useActionData();
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   const cart = useSelector(getCart);
   const finalPrice = useSelector(totalPrice);
+  const userName = useSelector(state => state.user.userName);
   const { status, position, address, error } = useSelector(state => state.user)
-  console.log(address);
+
+  const isSubmitting = navigation.state === "submitting";
+  const isgettingAddress = status === 'loading';
 
   const itemStyle = "flex flex-col justify-between sm:items-center sm:flex-row gap-2 sm:gap-0";
   const checkBox = "accent-yellow-400 h-6 w-6 focus-outline-none focus:ring focus:ring-yellow-200 focus:ring-offset-1";
-
-  const userName = useSelector(state => state.user.userName);
-  const dispatch = useDispatch();
 
   if (cart.length < 1) return <EmptyCart />;
 
@@ -59,22 +57,35 @@ function CreateOrder() {
         </div>
 
 
-        <div className={`${itemStyle} relative`}>
-          <label className="sm:basis-40">Address</label>
-          <input
-            type="text"
-            name="address"
-            required
-            defaultValue={address}
-            className="input sm:grow"
-          />
+        <div className="flex flex-col justify-between sm:items-center sm:flex-row gap-2 sm:gap-0 relative">
 
-          <span className="absolute z-50 sm:right-[5px] sm:top-[5px] right-[5px] top-[37px]">
-            <Button type="small" onClick={(e) => {
-              e.preventDefault();
-              dispatch(fetchAddress())
-            }}>get position</Button>
-          </span>
+          <label className="sm:basis-40">Address</label>
+          <div className="flex flex-col sm:grow sm:flex sm:flex-col">
+            <input
+              type="text"
+              name="address"
+              required
+              defaultValue={address}
+              className="input"
+            />
+
+            {
+              error !== ""
+              && <p className="text-xs p-2 my-1 text-red-800 bg-red-100 rounded-lg sm:w-full w-[297px]">{error}
+              </p>
+            }
+          </div>
+
+          {
+            address === ""
+            && <span className="absolute z-50 sm:right-[5px] sm:top-[5px] right-[5px] top-[37px] md:top-[6.5px]">
+              <Button type="small" onClick={(e) => {
+                e.preventDefault();
+                dispatch(fetchAddress())
+              }}>get position
+              </Button>
+            </span>
+          }
         </div>
 
         <div className="mt-6 mb-3 flex gap-3">
@@ -90,13 +101,20 @@ function CreateOrder() {
         </div>
 
         <div>
+          {/* Sending the desired data with type=hidden. this is a trick of sending data with the Form, so we can use it there in our order */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          {
+            (position.latitude && position.longitude)
+              ? <input type="hidden" name="position" value={JSON.stringify(position)} />
+              : null
+          }
+
+          <Button disabled={isSubmitting || isgettingAddress} type="primary">
             {isSubmitting ? "Placing Order..." : withPriority ? `Order now: pay ${finalPrice + Math.floor(finalPrice * 0.2)}` : `Order now: pay ${finalPrice}`}
           </Button>
         </div>
-      </Form>
-    </div>
+      </Form >
+    </div >
   );
 }
 
