c32c71a454d3fd74ae1537685967df0cc8b0097a
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-07 14:05:16 +0530

    900

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,7 +7,7 @@ function Button({ children, disabled, to, type, onClick }) {
   const style = {
     primary: base + " py-3 px-4 md:px-6 md:py-4",
     small: base + " py-2 px-4 md:px-5 md:py-2.5 text-xs",
-    secondary: "font-semibold text-stone-700 hover:bg-stone-200 border-4 border-stone-200 transition-colors duration-300 tracking-wide uppercase rounded-full focus: outline-none focus:ring focus: ring-yellow-300 focus: ring-offset-2 py-2 px-4 md:px6 md:py-4 hover:text-stone-500",
+    secondary: "font-semibold text-stone-700 hover:bg-stone-200 border-4 border-stone-200 transition-colors duration-300 tracking-wide uppercase rounded-full focus: outline-none focus:ring focus: ring-yellow-300 focus: ring-offset-2 py-2 px-4 md:px6 md:py-4 hover:text-stone-900",
     round: base + "py-1 px-2 text-sm md:py-2 md:px-4",
   }
 
