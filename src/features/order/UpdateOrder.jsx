969f97d0ef65c00a8a9f281f190a40156a8d8c4d
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-08 11:50:11 +0530

    FORM

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,14 +1,20 @@
-import { useFetcher } from "react-router-dom";
+import { Form, useFetcher } from "react-router-dom";
 import Button from "../../ui/Button";
 
 function UpdateOrder() {
-  const fetcher = useFetcher();
+  // const fetcher = useFetcher();
 
   return (
-    <fetcher.Form method="PATCH" className="text-right mt-6">
+    <Form method="PATCH" className="text-right mt-4">
       <Button type="small">Make Priority</Button>
-    </fetcher.Form>
+    </Form>
   )
+}
+
+export const action = function ({ request, params }) {
+  console.log('update');
+
+  return null;
 }
 
 export default UpdateOrder;
